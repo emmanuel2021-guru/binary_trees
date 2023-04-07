1457d4694d994c36a2c91906a99e85326d6f9a79
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-07 18:36:12 +0100

    Handled malloc return

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL)
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
