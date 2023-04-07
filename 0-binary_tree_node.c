9e9baea9a2466d50f3a727f143b2c2d733b53799
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-07 18:38:39 +0100

    Fixed syntax

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
-		return (NULL)
+		return (NULL);
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
